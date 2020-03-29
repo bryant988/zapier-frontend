10104ed0c6181ac9e47e98ff97bc359c1585568d
bgallardo <bgallardo@connectwise.com>
2020-03-29 00:09:06 -0400

    add sass support

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import css from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css["App"]}>
+      <header className={css["App-header"]}>
+        <img src={logo} className={css["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={css["App-link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
